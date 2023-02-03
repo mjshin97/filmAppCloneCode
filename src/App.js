eea262dd9931dcde6fbd58b06930681f2378db00
Minjun Shin <mjshin97@naver.com>
2023-02-03 22:44:15 +0900

    31th commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("call an api");
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("Call the API");
+  }, []);
+  /*
+  const iRunOnlyOnce = () => {
+    console.log('i run only once');
+  };
+  useEffect(iRunOnlyOnce, []);
+  */
   return (
     <div>
       <h1>{counter}</h1>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-//#6.0 Introduction (05:51)
+//#6.1 useEffect (04:57)
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
