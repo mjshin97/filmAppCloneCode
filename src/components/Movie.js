2bf9bcdec46bc417a30edade8ac081a6ad3f5ff5
Minjun Shin <mjshin97@naver.com>
2023-02-06 20:45:08 +0900

    41th commit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,25 @@
 import PropTypes from "prop-types";
 import {Link}from "react-router-dom";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
       <h2>
-        <Link to="/movie">{title}</Link>
+        <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
           <li key={g}>{g}</li>
         ))}
-      </ul>
+      </ul>   
     </div>
   );
 }
 
 Movie.propTypes = {
+  id:PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//#7.5 React Router (11:29)
+//#7.6 Parameters (11:54)
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
